f7d5685a4ad425ed05044ce7b789f65093478097
yuhang-z <yuhang.zhang@mail.mcgill.ca>
2021-08-21 15:47:33 -0400

    all methods completed and updated

--- src/main/java/com/citi/hackathon/PortfolioManager/services/AccountService.java
+++ src/main/java/com/citi/hackathon/PortfolioManager/services/AccountService.java
@@ -3,14 +3,19 @@ import com.citi.hackathon.PortfolioManager.entities.Account;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountService {
     Collection<Account> getAllAccounts();
+    Collection<Account> getInvestment();
+    Collection<Account> getCash();
     Optional<Account> getAccountById(Integer id);
     HashMap<String, Double> getAccountValue();
     double getStockPrice(String name);
     Collection<String> getGainers();
     Collection<String> getLosers();
+    Collection<Double> getIndices();
+    double getPastNetWorth(String time);
 }
 
