3e8332ee6ba31a60e3491022d6307613a7bb5dce
yuhang-z <yuhang.zhang@mail.mcgill.ca>
2021-08-25 13:46:10 -0400

    Update AccountController.java

--- src/main/java/com/citi/hackathon/PortfolioManager/rest/AccountController.java
+++ src/main/java/com/citi/hackathon/PortfolioManager/rest/AccountController.java
@@ -23,25 +23,25 @@ public class AccountController {
 
 
     @GetMapping
-    @CrossOrigin(origins = "http://citihackathon-citihackathon.namdevops31.conygre.com")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Account> getAccounts() {
         return accountService.getAllAccounts();
     }
 
     @GetMapping("/investment")
-    @CrossOrigin(origins = "http://citihackathon-citihackathon.namdevops31.conygre.com")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Account> getInvestment() {
         return accountService.getInvestment();
     }
 
     @GetMapping("/cash")
-    @CrossOrigin(origins = "http://citihackathon-citihackathon.namdevops31.conygre.com")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Account> getCash() {
         return accountService.getCash();
     }
 
     @GetMapping("/{id}")
-    @CrossOrigin(origins = "http://citihackathon-citihackathon.namdevops31.conygre.com")
+    @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<Account> getAccountById(@PathVariable("id") Integer id) {
         Optional<Account> account = accountService.getAccountById(id);
         if(account.isPresent()){
@@ -53,7 +53,7 @@ public class AccountController {
     }
 
     @GetMapping("/value")
-    @CrossOrigin(origins = "http://citihackathon-citihackathon.namdevops31.conygre.com")
+    @CrossOrigin(origins = "http://localhost:4200")
     public HashMap<String, Double> getValuesInformation() {
         //HashMap<String, Double> valInfo = accountService.getAccountValue();
         //return new String("Net Worth" + String.valueOf(valInfo.get("Net Worth")));
@@ -61,25 +61,25 @@ public class AccountController {
     }
 
     @GetMapping("/gainers")  
-    @CrossOrigin(origins = "http://citihackathon-citihackathon.namdevops31.conygre.com")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<String> getGainers() {
         return accountService.getGainers();
     }
 
     @GetMapping("/losers")
-    @CrossOrigin(origins = "http://citihackathon-citihackathon.namdevops31.conygre.com")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<String> getLosers() {
         return accountService.getLosers();
     }
 
     @GetMapping("/indices")
-    @CrossOrigin(origins = "http://citihackathon-citihackathon.namdevops31.conygre.com")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Double> getIndices() {
         return accountService.getIndices();
     }
 
     @GetMapping("/past/{time}")
-    @CrossOrigin(origins = "http://citihackathon-citihackathon.namdevops31.conygre.com")
+    @CrossOrigin(origins = "http://localhost:4200")
     public double getPastNetWorth(@PathVariable("time") String time) {
         double pastNetWorth = accountService.getPastNetWorth(time);
         return pastNetWorth;
